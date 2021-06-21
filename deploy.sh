53062eb6e7731621d14cb098794e506941e825f7
jamieshui <jamie_shui@yeah.net>
2021-06-21 15:07:48 +0800

    :adhesive_bandage: 0046 revised

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git add -A
 
 # 修改具体题目编号
-git commit -m "::adhesive_bandage:: 0046 revised "
+git commit -m ":adhesive_bandage: 0046 revised "
 
 git config --global http.sslVerify "false"
 git config --global --unset http.proxy
